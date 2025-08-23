d185516e877500a5e798065f6bec10c04f9e7289
Muhammad Atif <atifahmadani68@gmail.com>
2025-08-23 15:13:53 +0500

    Pick a Random Card

---
+++ random_card.py
@@ -0,0 +1,7 @@
+import random
+cards=random.choice(['Diamonds','Spades','Hearts','clubs'])
+ranks=random.choice([2,3,4,5,6,7,8,9,10,'jack','King','Queen','ACE'])
+
+print('..............')
+print(f'The {ranks} of {cards}')
+print('..............')
